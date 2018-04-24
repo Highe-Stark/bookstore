f87b22e802d44a495788e13c4eedb33cfb0affb3
Higher Stark <higherwashington@gmail.com>
2018-04-25 00:27:01 +0800

    A small step to card.

--- src/js/App.js
+++ src/js/App.js
@@ -8,30 +8,37 @@ import Book from '../img/Books-2.jpg';
 class App extends React.Component {
     render() {
         return (
-            <Segment.Group horizontal compact>
-                <Segment floated='left'>
-                    <Header as="h2">
-                        <Image circular src={Book} height='20rem' verticalAlign='middle'/>
-                        {' '}书轩
+            <div>
+                <div>
+                    <Segment.Group horizontal compact>
+                        <Segment floated='left'>
+                            <Header as="h2">
+                                <Image circular src={Book} height='20rem' verticalAlign='middle' />
+                                {' '}书轩
                         <Header.Subheader>
-                            <p>Enjoy Book, Enjoy Life.</p>
-                        </Header.Subheader>
-                    </Header>
-                </Segment>
-                <Segment>
-                    <Input icon={<Icon name='search' inverted circular link/>}
-                    placeholder="Search..." fluid focus
-                    />
-                </Segment>
-                <Segment textAlign='center' floated='right'>
-                    <Dropdown icon='user' floating labeled button>
-                        <Dropdown.Menu>
-                            <Dropdown.Item text="Sign in" />
-                            <Dropdown.Item text="Sign up" />
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Segment>
-            </Segment.Group>
+                                    <p>Enjoy Book, Enjoy Life.</p>
+                                </Header.Subheader>
+                            </Header>
+                        </Segment>
+                        <Segment>
+                            <Input icon={<Icon name='search' inverted circular link />}
+                                placeholder="Search..." fluid focus
+                            />
+                        </Segment>
+                        <Segment textAlign='center' floated='right'>
+                            <Dropdown icon='user' floating labeled button>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item text="Sign in" />
+                                    <Dropdown.Item text="Sign up" />
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Segment>
+                    </Segment.Group>
+                </div>
+                <div>
+                    <Home />
+                </div>
+            </div>
         );
     }
 }
--- src/js/home/home.js
+++ src/js/home/home.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {Dropdown} from 'semantic-ui-react';
+import {Grid, Button, Card, Image} from 'semantic-ui-react';
+import Le_Petit_Prince from '../../img/Le_Petit_Prince.jpg';
+import Bible from '../../img/Bible.jpg';
+
+var books = [[Le_Petit_Prince, "Prince"], [Bible, 'Jesus']];
 
 class Home extends React.Component {
-    /* constructor(props) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            dropdownOpen : false
-        };
-    }
-
-    toggle() {
-        this.setState(prevState => ({
-            dropdownOpen: !prevState.dropdownOpen
-        }));
-    }*/
+    
 
     render() {
         return (
-            <Dropdown text='Account' icon='user' floating labeled button className='icon'>
-                <Dropdown.Menu>
-                    <Dropdown.Item text="Sign in"/>
-                    <Dropdown.Item text="Sign up"/>
-                </Dropdown.Menu>
-            </Dropdown>
-        )
+            /*<Grid column={3} divided>
+                {books.map(book => (
+                    <Grid.Column key={book[1]}>
+                        <Card>
+                            <Image src={book[0]} rounded />
+                            <Card.Content>
+                                {book[1]}
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                ))}
+            </Grid>*/
+            <Card.Group>
+                {books.map(book => (
+                    <Card>
+                        <Image src={book[0]} size="small" rounded />
+                        <Card.Content>
+                            <Card.Header>
+                                {book[1]}
+                            </Card.Header>
+                        </Card.Content>
+                    </Card>
+                ))}
+            </Card.Group>
+        );
     }
 }
 
